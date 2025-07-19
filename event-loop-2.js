823eb8080e130b5e22c99be34b82458a69ecac00
Shruthi KV <shruthikv@Mac.lan>
2025-07-19 20:48:07 +0530

    commit -2

--- event-loop-2.js
+++ event-loop-2.js
@@ -14,6 +14,14 @@ fs.readFile('./textFile.txt', "utf-8", ()=>{
     console.log("file read completed")
 })
 
+setTimeout(()=>{
+    console.log("setTimeout - done")
+}, 0)
 
+process.nextTick(()=> console.log("Process.nextTick"))
+
+function printA(){
+    console.log(a)
+}
 
 printA()
